8ac2c8c59f233ce91c9860b8cc291e7f3bd9d2b3
Casey McGuire <caseyjaymcguire@gmail.com>
2018-07-15 10:33:15 -0700

    Added gulpfile so that I could create a gulp task that watches our handlebars files and moves them to the dist folder when they change (since the TypeScript compiler doesn't do this by default).

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+
+//source: https://blog.andrewray.me/how-to-copy-only-changed-files-with-gulp/
+const sourceFolder = "./src/server/views";
+const destinationFolder = "./dist/server/views";
+
+gulp.task('watch-handlebars', () => {
+  gulp.src(sourceFolder + '/**/*', {base: sourceFolder})
+    .pipe(watch(sourceFolder, {base: sourceFolder}))
+    .pipe(gulp.dest(destinationFolder));
+});
