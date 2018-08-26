1b0dca27a48d669d4bd46836b152721b4c6d8996
Casey McGuire <caseyjaymcguire@gmail.com>
2018-08-25 19:31:27 -0700

    Made it so that if a database connection string is provided, we use that to connect to the database instead of the environment variables.
    It turns out that heroku automatically creates a user so we don't need to do so ourselves.

--- src/server/db/PostgresDatabaseManagerImpl.ts
+++ src/server/db/PostgresDatabaseManagerImpl.ts
@@ -6,12 +6,22 @@ export default class PostgresDatabaseManagerImpl implements DatabaseManager {
   private readonly pool: Pool;
 
   constructor() {
-    this.pool = new Pool({
-      user: process.env.MY_SITE_USER,
-      host: process.env.MY_SITE_HOST,
-      database: process.env.MY_SITE_DATABASE,
-      password: process.env.MY_SITE_PASSWORD
-    });
+    let config;
+    if (process.env.DATABASE_URL) {
+      config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+      }
+    }
+    else {
+      config = {
+        user: process.env.MY_SITE_USER,
+        host: process.env.MY_SITE_HOST,
+        database: process.env.MY_SITE_DATABASE,
+        password: process.env.MY_SITE_PASSWORD
+      }
+    }
+    this.pool = new Pool(config);
   }
 
   query(text: string, params: any[], callback: (err: Error | undefined, result?: any[]) => void) {
